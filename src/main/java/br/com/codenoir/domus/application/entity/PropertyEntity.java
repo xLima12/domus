e979596552e18d8b31b553f022add6fe83c75b7b
xLima12 <felipelima1910@gmail.com>
2025-04-29 17:04:15 -0300

    refactor: Improve readability of entities with separation of variables by line

--- src/main/java/br/com/codenoir/domus/application/entity/PropertyEntity.java
+++ src/main/java/br/com/codenoir/domus/application/entity/PropertyEntity.java
@@ -19,24 +19,31 @@ public class PropertyEntity {
     @NotBlank(message = "Name cannot blank")
     @NotNull(message = "Name cannot null")
     private String name;
+
     @NotBlank(message = "Address cannot blank")
     @NotNull(message = "Address cannot null")
     private String address;
+
     @Min(value = 0, message = "The number to bedrooms should not be less than 0 (zero).")
     @Min(value = 50, message = "The number to bedrooms should not be more than 50 (fifty).")
     private int bedrooms;
+
     @Min(value = 0, message = "The number to living room should not be less than 0 (zero).")
     @Min(value = 50, message = "The number to living room should not be more than 50 (fifty).")
     private int livingRoom;
+
     @Min(value = 0, message = "The number to kitchen should not be less than 0 (zero).")
     @Min(value = 50, message = "The number to kitchen should not be more than 50 (fifty).")
     private int kitchen;
+
     @Min(value = 0, message = "The number to bathroom should not be less than 0 (zero).")
     @Min(value = 50, message = "The number to bathroom should not be more than 50 (fifty).")
     private int bathroom;
+
     @Min(value = 0, message = "The number to garage should not be less than 0 (zero).")
     @Min(value = 50, message = "The number to garage should not be more than 50 (fifty).")
     private int garage;
+
     @Min(value = 0, message = "The number to basement should not be less than 0 (zero).")
     @Min(value = 50, message = "The number to basement should not be more than 50 (fifty).")
     private int basement;
