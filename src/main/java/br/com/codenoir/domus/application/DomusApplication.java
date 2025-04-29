d04ef1f59a7f2c6a549e231837e2a02cd0d30b1e
xLima12 <felipelima1910@gmail.com>
2025-04-28 23:37:47 -0300

    feat: configuration of Domus and Domus Gateway applications with basic security

--- src/main/java/br/com/codenoir/domus/application/DomusApplication.java
+++ src/main/java/br/com/codenoir/domus/application/DomusApplication.java
@@ -1,4 +1,4 @@
-package br.com.codenoir.domus;
+package br.com.codenoir.domus.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/br/com/codenoir/domus/gateway/DomusGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.codenoir.domus.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomusGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomusGatewayApplication.class, args);
+    }
+
+}
