092e023ee71bc8ad23c9581a95b5a1cfb44cb196
xLima12 <felipelima1910@gmail.com>
2025-05-09 15:28:48 -0300

    Refactor(auth): Add the authorization roles in the resolvers

--- src/main/java/br/com/codenoir/domus/gateway/controller/OfferPropertyResolver.java
+++ src/main/java/br/com/codenoir/domus/gateway/controller/OfferPropertyResolver.java
@@ -3,6 +3,9 @@ package br.com.codenoir.domus.gateway.controller;
 import br.com.codenoir.domus.application.offerProperty.dto.OfferPropertyRequestDTO;
 import br.com.codenoir.domus.application.offerProperty.entity.OfferPropertyEntity;
 import br.com.codenoir.domus.application.offerProperty.service.OfferPropertyService;
+import br.com.codenoir.domus.application.security.DomusGraphQLContext;
+import br.com.codenoir.domus.application.security.DomusRolesAllowed;
+import graphql.schema.DataFetchingEnvironment;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
@@ -18,30 +21,43 @@ import java.util.UUID;
 public class OfferPropertyResolver {
 
     @Autowired
-    OfferPropertyService offerPropertyService;
+    private OfferPropertyService offerPropertyService;
 
+    @DomusRolesAllowed(value = {"MASTER", "COMPANY", "ADMIN"})
     @QueryMapping
-    public Optional<OfferPropertyEntity> getOfferProperty(@Argument UUID id) {
+    public Optional<OfferPropertyEntity> getOfferProperty(@Argument UUID id, DataFetchingEnvironment environment) {
+        DomusGraphQLContext userContext = environment.getGraphQlContext().get("auth");
         return offerPropertyService.findById(id);
     }
 
+    @DomusRolesAllowed(value = {"MASTER", "COMPANY", "ADMIN"})
     @QueryMapping
-    public List<OfferPropertyEntity> getAllOfferProperties() {
+    public List<OfferPropertyEntity> getAllOfferProperties(DataFetchingEnvironment environment) {
+        DomusGraphQLContext userContext = environment.getGraphQlContext().get("auth");
         return offerPropertyService.findAll();
     }
 
+    @DomusRolesAllowed(value = {"MASTER", "COMPANY", "ADMIN"})
     @MutationMapping
-    public OfferPropertyEntity createOfferProperty(@Argument @Valid OfferPropertyRequestDTO input) {
+    public OfferPropertyEntity createOfferProperty(@Argument @Valid OfferPropertyRequestDTO input,
+                                                   DataFetchingEnvironment environment) {
+        DomusGraphQLContext userContext = environment.getGraphQlContext().get("auth");
         return offerPropertyService.create(input);
     }
 
+    @DomusRolesAllowed(value = {"MASTER", "COMPANY", "ADMIN"})
     @MutationMapping
-    public OfferPropertyEntity updateOfferProperty(@Argument UUID id, @Argument @Valid OfferPropertyRequestDTO input) {
+    public OfferPropertyEntity updateOfferProperty(@Argument UUID id,
+                                                   @Argument @Valid OfferPropertyRequestDTO input,
+                                                   DataFetchingEnvironment environment) {
+        DomusGraphQLContext userContext = environment.getGraphQlContext().get("auth");
         return offerPropertyService.update(id, input);
     }
 
+    @DomusRolesAllowed(value = {"MASTER", "COMPANY", "ADMIN"})
     @MutationMapping
-    public boolean deleteOfferProperty(@Argument UUID id) {
+    public boolean deleteOfferProperty(@Argument UUID id, DataFetchingEnvironment environment) {
+        DomusGraphQLContext userContext = environment.getGraphQlContext().get("auth");
         return offerPropertyService.delete(id);
     }
 
